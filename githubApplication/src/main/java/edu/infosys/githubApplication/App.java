e629c1125aee8b9b5f3ab0173f4cdc470d6141eb
VAISHNAVI <VAISHNAVI@LAPTOP-6N5SM13J>
2026-03-20 16:21:48 +0530

    First Version

---
+++ githubApplication/src/main/java/edu/infosys/githubApplication/App.java
@@ -0,0 +1,10 @@
+package edu.infosys.githubApplication;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ githubApplication/src/main/java/edu/infosys/githubApplication/Welcome.java
@@ -0,0 +1,10 @@
+package edu.infosys.githubApplication;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.print("welcome to github Demo");
+
+	}
+
+}
